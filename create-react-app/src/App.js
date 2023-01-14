1ba9be3c96696d67a5c85cbb2bcbf1a594f98cce
BorislavChernev <borocool359@gmail.com>
2023-01-14 14:43:09 +0200

    Update

--- create-react-app/src/App.js
+++ create-react-app/src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
+import Footer from './footer';
 import './App.css';
+import Header from './header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Modified this line aaa
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ create-react-app/src/header.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+const Header = () => {
+	return (
+		<header className="App-header">
+			<h1>Hi React!</h1>
+			<img src={logo} className="App-logo" alt="logo" />
+		</header>
+	);
+};
+
+export default Header;
