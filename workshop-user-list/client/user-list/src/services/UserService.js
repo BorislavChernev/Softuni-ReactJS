bafbb6cc6dec0bbafa24dbdeb9c68b155fc4b5db
BorislavChernev <borocool359@gmail.com>
2023-04-09 12:14:34 +0300

    Created UserServicesJS

---
+++ workshop-user-list/client/user-list/src/services/UserService.js
@@ -0,0 +1,29 @@
+const baseUrl = 'http://localhost:3005/api/users';
+
+export const getAll = async () => {
+	const response = await fetch(baseUrl);
+	const result = await response.json();
+
+	return result.users;
+};
+
+export const getOne = async (userId) => {
+	const response = await fetch(`${baseUrl}/${userId}`);
+	const result = await response.json();
+
+	return result.user;
+};
+
+export const create = async (userData) => {
+	const response = await fetch(baseUrl, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify(userData),
+	});
+
+	const result = await response.json();
+	console.log(result);
+	return result.user;
+};
